c3e6029f06d8b9f9282d9b37be3d0a6eebafc865
t-kojima <cran-berry.9@hotmail.co.jp>
2017-10-11 13:25:34 +0900

    use generator

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,12 +1,16 @@
 class BonusDrink
   def self.total_count_for(amount)
-    count = amount
-    while amount > 2 do
-      bonus = (amount / 3).floor
-      amount = (amount - bonus * 3) + bonus
-      count += bonus                    
+    get_generator(amount).inject(:+)
+  end
+
+  def self.get_generator(value)
+    Enumerator.new do | g |
+      g << value
+      while value > 2 do
+        g << value.div(3)
+        value = value % 3 + value.div(3)
+      end
     end
-    return count
   end
 end
 
--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -4,16 +4,21 @@ describe BonusDrink do
   
   specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
 
-  it 'amount 1 return 1' do
+  it 'input 1 to 13' do
     expect(BonusDrink.total_count_for(1)).to eq 1
-  end
-
-  it 'amount 3 return 4' do
+    expect(BonusDrink.total_count_for(2)).to eq 2
     expect(BonusDrink.total_count_for(3)).to eq 4
-  end
-
-  it 'amount 11 return 16' do
+    expect(BonusDrink.total_count_for(4)).to eq 5
+    expect(BonusDrink.total_count_for(5)).to eq 7
+    expect(BonusDrink.total_count_for(6)).to eq 8
+    expect(BonusDrink.total_count_for(7)).to eq 10
+    expect(BonusDrink.total_count_for(8)).to eq 11
+    expect(BonusDrink.total_count_for(9)).to eq 13
+    expect(BonusDrink.total_count_for(10)).to eq 14
     expect(BonusDrink.total_count_for(11)).to eq 16
+    expect(BonusDrink.total_count_for(12)).to eq 17
+    expect(BonusDrink.total_count_for(13)).to eq 19
+    expect(BonusDrink.total_count_for(14)).to eq 20
   end
 
 end
