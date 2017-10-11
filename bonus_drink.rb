594e300a84c2be091fcf2712bbcdb6c97268a6c0
chibo <cran-berry.9@hotmail.co.jp>
2017-10-11 21:01:07 +0900

    recursive

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,17 +1,18 @@
 class BonusDrink
+
   def self.total_count_for(amount)
-    get_generator(amount).inject(:+)
+    amount + get_bonus(amount)
   end
 
-  def self.get_generator(value)
-    Enumerator.new do | g |
-      g << value
-      while value > 2 do
-        g << value.div(3)
-        value = value % 3 + value.div(3)
-      end
+  def self.get_bonus(value)
+    bonus = value.div(3)
+    if bonus == 0
+      0
+    else
+      get_bonus(value % 3 + bonus) + bonus
     end
   end
+
 end
 
 puts BonusDrink.total_count_for(ARGV[0].to_i)
